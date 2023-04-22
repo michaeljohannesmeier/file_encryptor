0d65ed185c3229e676f7417e60aaca19f40d27bd
Michael Meier <michael_johannes.meier@daimler.com>
2023-04-23 01:18:17 +0200

    path from outside

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,7 @@
 """FILE-ENCRYPTOR CLI main."""
 
 import sys
+import pathlib
 
 import click
 from loguru import logger
@@ -12,6 +13,9 @@ from .crypto import de_or_encrypt, gen_new_key
 logger.remove()
 logger.add(sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>", level="INFO")
 
+def get_project_root_path():
+    return pathlib.Path(__file__).parent.parent.parent.absolute()
+
 @click.group()
 def file_encryptor():
     """FILE-ENCRYPTOR CLI HELPER."""
@@ -20,22 +24,25 @@ def file_encryptor():
 @click.command()
 @click.option("--file", "-f", help="File to decrypt - defaults to all files specified in crypto.py")
 @click.option("--key", "-k", help="Fernet key - defaults to value in decrypt.key file")
-def decrypt(file, key):
+@click.option("--path", "-p", help="Path of project folder where files should be decrypted", default=get_project_root_path())
+def decrypt(path, file, key):
     """Decrypts all files specified in crypto.py"""
-    de_or_encrypt("decrypt", file, key)
+    de_or_encrypt(path, "decrypt", file, key)
 
 @click.command()
 @click.option("--file", "-f", help="File to encrypt - defaults to all files specified in crypto.py")
 @click.option("--key", "-k", help="Fernet key - defaults to value in decrypt.key file")
-def encrypt(file, key):
+@click.option("--path", "-p", help="Path of project folder where files should be decrypted", default=get_project_root_path())
+def encrypt(path, file, key):
     """Encrypts all files specified in crypto.py"""
-    de_or_encrypt("encrypt", file, key)
+    de_or_encrypt(path, "encrypt", file, key)
 
 @click.command()
-@click.option("--filepath", "-f", help="Filepath to save new key to", default="decrypt.key")
-def generate_new_key(filepath):
+@click.option("--path", "-p", help="Folder path to save new key to", default=get_project_root_path())
+@click.option("--filename", "-f", help="Filename to save new key to", default="decrypt.key")
+def generate_new_key(path, filename):
     """Encrypts all files specified in crypto.py"""
-    gen_new_key(filepath)
+    gen_new_key(path, filename)
 
 
 
--- src/crypto.py
+++ src/crypto.py
@@ -9,11 +9,11 @@ from cryptography.fernet import Fernet
 
 
 
-PROJECT_ROOT_FOLDER = os.path.join(pathlib.Path(__file__).parent.parent.parent.parent.parent.parent.absolute())
 
-with open(os.path.join(PROJECT_ROOT_FOLDER, "config.yml")) as stream:
-    config = yaml.safe_load(stream)
-    FILES_TO_ENCRYPT = config["file-encryptor"]["files-to-encrypt"]
+def get_files_to_encrypt(project_root_folder: str):
+    with open(os.path.join(project_root_folder, "config.yml")) as stream:
+        config = yaml.safe_load(stream)
+        return config["file-encryptor"]["files-to-encrypt"]
 
 
 
@@ -21,21 +21,21 @@ class Mode(Enum):
     DECRYPT = "decrypt"
     ENCRYPT = "encrypt"
 
-def init_fernet(key = None):
+def init_fernet(project_root_folder: str, key = None):
     if not key:
         logger.info("Using key from file: decrypt.key")
-        with open(os.path.join(PROJECT_ROOT_FOLDER, 'decrypt.key'), 'rb') as filekey:
+        with open(os.path.join(project_root_folder, 'decrypt.key'), 'rb') as filekey:
             key = filekey.read()
     return Fernet(key)
 
-def de_or_encrypt(de_or_encrypt: str, file = None, key = None):
-    fernet = init_fernet(key)
+def de_or_encrypt(project_root_folder: str, de_or_encrypt: str, file = None, key = None):
+    fernet = init_fernet(project_root_folder, key)
     count = 0
     if file:
         files_to_encrypt = [file]
     else:
-        files_to_encrypt = FILES_TO_ENCRYPT
-    for root, _, filenames in os.walk(PROJECT_ROOT_FOLDER):
+        files_to_encrypt = get_files_to_encrypt(project_root_folder)
+    for root, _, filenames in os.walk(project_root_folder):
         for filename in filenames:
             for file_to_encrypt in files_to_encrypt:
                 filepath = os.path.join(root, filename)
@@ -55,9 +55,9 @@ def de_or_encrypt(de_or_encrypt: str, file = None, key = None):
                     encrypted_file.write(de_or_encrypted)
     logger.info(f"Successfully {de_or_encrypt}ed: {count} files")   
 
-def gen_new_key(filename: str):
+def gen_new_key(project_root_folder: str, filename: str = "decrypt.key"):
     key = Fernet.generate_key()
-    file_path = os.path.join(PROJECT_ROOT_FOLDER, "file-encryptor", filename)
+    file_path = os.path.join(project_root_folder, filename)
     with open(file_path, 'wb') as filekey:
       logger.info(f"Saving key to {file_path}")
       filekey.write(key)
